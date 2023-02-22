26274232d7a6faa7b5a7c25664a396c91b71a569
Mohammad-Shahzada <111749397+Mohammad-Shahzada@users.noreply.github.com>
2023-02-22 15:57:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,3 +5,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-01a4f99c4ac11b03c" # ap-south-1
   instance_type = "t2.micro"
 }
+resource "aws_eip" "res1" {
+  vpc      = true
+  provider = aws.default
+}
